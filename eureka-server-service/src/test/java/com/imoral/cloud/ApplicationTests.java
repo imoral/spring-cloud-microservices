f7cdb9f5193cdb331108c478f462470c6f45c9e4
imoral <imoral@192.168.0.11>
2016-05-14 23:57:45 +0200

    Docker file and deploy configured

--- eureka-server-service/src/test/java/com/imoral/cloud/ApplicationTests.java
+++ eureka-server-service/src/test/java/com/imoral/cloud/ApplicationTests.java
@@ -3,10 +3,14 @@ package com.imoral.cloud;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
+@WebIntegrationTest
+@DirtiesContext
 public class ApplicationTests {
 
 	@Test
